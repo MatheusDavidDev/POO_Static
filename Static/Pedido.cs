62377ad3f8377e2d735ce85f3dbb95fa38cbe2f0
matheus <matheusviniciusdavid@gmail.com>
2020-12-14 08:48:53 -0300

    Aula feita

---
+++ Static/Pedido.cs
@@ -0,0 +1,7 @@
+namespace Static
+{
+    public class Pedido
+    {
+        
+    }
+}
---
+++ Static/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Aula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instanciar e ? // Conversor c = new Conversor();
+            // Chamamos nossa classe estatica, sem precisar instanciar
+            Console.WriteLine( Conversor.DolarParaReal(4f) );
+
+            Console.WriteLine( Conversor.RealParaDolar(50f) );
+            
+            // Exemplo: Classe Math
+            Console.WriteLine( Math.Pow(2 , 4) );
+
+            Console.WriteLine( Conversor.EuroParaReal(10F) );
+
+            Console.WriteLine( Conversor.RealParaEuro(100f) );
+
+        
+        }
+    }
+}
